684596ff9e7afe1e95d2c17c9a440f3149078a85
qazaleh <qkkalhori@gmail.com>
2024-07-30 16:24:29 +0200

    Federify added

--- Setup/setup.py
+++ Setup/setup.py
@@ -5,18 +5,16 @@ import subprocess
 compile  = subprocess.run(['zokrates compile -i main.zok'], stdout=subprocess.PIPE, shell=True, text=True)
 print("compute compile result", compile.stdout)
 
-constraints  = subprocess.run(['zokrates inspect'], stdout=subprocess.PIPE, shell=True, text=True)
-print(constraints.stdout)
+# constraints  = subprocess.run(['zokrates inspect'], stdout=subprocess.PIPE, shell=True, text=True)
+# print(constraints.stdout)
 
-# setup = subprocess.run(['zokrates setup'], stdout=subprocess.PIPE, shell=True, text=True)
-# print("compute setup result", setup.stdout)
+setup = subprocess.run(['zokrates setup'], stdout=subprocess.PIPE, shell=True, text=True)
+print("compute setup result", setup.stdout)
 
 
-subprocess.run(['mkdir 100_5_90'], stdout=subprocess.PIPE, shell=True, text=True)
+setup = subprocess.run(['zokrates export-verifier'], stdout=subprocess.PIPE, shell=True, text=True)
+print("compute setup result", setup.stdout)
 
-subprocess.run(['cp out ./100_5_90'], stdout=subprocess.PIPE, shell=True, text=True)
-subprocess.run(['cp out.ztf ./100_5_90'], stdout=subprocess.PIPE, shell=True, text=True)
-# subprocess.run(['cp proving.key ./100_5_55'], stdout=subprocess.PIPE, shell=True, text=True)
-# subprocess.run(['cp verification.key ./100_5_55'], stdout=subprocess.PIPE, shell=True, text=True)
 
+subprocess.run(['cp verifier.sol ../Contracts'], stdout=subprocess.PIPE, shell=True, text=True)
 
